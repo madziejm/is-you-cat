2a7d49a66219bba27a728e21844995a6071683d7
Sahcim <307275@uwr.edu.pl>
2021-06-01 16:58:42 +0200

    dataset download script update

--- cat-fetcher/get-data.sh
+++ cat-fetcher/get-data.sh
@@ -1,21 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-
-DIR="Cats_Dataset"
-if [ -d "$DIR" ]; then
-    echo "$DIR exists"
-    exit 0
-fi
-mkdir $DIR
-cd $DIR
-echo "Downloading Dataset"
+pip install openimages
+oi_download_images --base_dir temp --limit 26 --labels "Rose" "Flag" "Flashlight" "Sea turtle" "Camera" "Animal" "Glove" "Crocodile" "Cattle" "House" "Guacamole" "Penguin" "Vehicle registration plate" "Bench" "Ladybug" "Human nose" "Watermelon" "Flute" "Butterfly" "Washing machine" "Raccoon" "Segway" "Taco" "Jellyfish" "Cake" "Pen" "Cannon" "Bread" "Tree" "Shellfish" "Bed" "Hamster" "Hat" "Toaster" "Sombrero" "Tiara" "Bowl" "Dragonfly" "Moths and butterflies" "Antelope" "Vegetable" "Torch"
+mkdir niekotki
+find . -wholename "./temp/*/*.jpg" -exec cp {} niekotki/ \;
+rm -rf temp
 wget -q --show-progress https://download.microsoft.com/download/3/E/1/3E1C3F21-ECDB-4869-8368-6DEBA77B919F/kagglecatsanddogs_3367a.zip
-echo -ne "Unziping..."
-unzip -qq -j kagglecatsanddogs_*.zip 'PetImages/Cat/*' -d Images && rm kagglecatsanddogs_*.zip
-rm Images/Thumbs.db 
-echo " OK" 
-echo -ne "Generating txt file"
-touch images.txt
-ls Images > images.txt
-echo " OK"
-exit 1
+unzip -qq -j kagglecatsanddogs_*.zip 'PetImages/Cat/*' -d temp && rm kagglecatsanddogs_*.zip
+rm temp/666.jpg
+rm temp/Thumbs.db
+python3 split-cats.py
+mkdir Train Val Test
+mv cats_1 Train/Cat
+mv cats_2 Val/Cat
+mv cats_3 Test/Cat
+mv niekotki Test/NCat
